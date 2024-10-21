d527810a0d8169ae2b2c9c097ad5c75c76e9bc5c
cardorimezgo <cardori.mezgo@gmail.com>
2024-10-20 22:06:08 -0600

    Add files via upload

--- src/Physics/Force.cpp
+++ src/Physics/Force.cpp
@@ -1,86 +1,85 @@
-#include "Force.h"
+#include "./Force.h"
 #include <algorithm>
 
-Vec2 Force::GenerateDragForce(const Particle& particle, float k){
-    Vec2 dragForce = Vec2(0,0);
-    if(particle.velocity.MagnitudeSquared() > 0){
-        //calculate the drag direction(inverse of velocity unit vector)
+Vec2 Force::GenerateDragForce(const Particle& particle, float k) {
+    Vec2 dragForce = Vec2(0, 0);
+    if (particle.velocity.MagnitudeSquared() > 0) {
+        // Calculate the drag direction (inverse of velocity unit vector)
         Vec2 dragDirection = particle.velocity.UnitVector() * -1.0;
 
-        //Calculate the drag magnitude, k * |v|^2
-        float dragMagnitud = k * particle.velocity.MagnitudeSquared();
+        // Calculate the drag magnitude, k * |v|^2
+        float dragMagnitude = k * particle.velocity.MagnitudeSquared();
 
-        //generate the final drag force with direction and magnitude
-        dragForce = dragDirection * dragMagnitud;
+        // Generate the final drag force with direction and magnitude
+        dragForce = dragDirection * dragMagnitude;
     }
     return dragForce;
 }
 
-
-Vec2 Force::GenerateFrictionForce(const Particle& particle, float k){
-    Vec2 frictionForce = Vec2(0,0);
-
-    //Calculate the friction direction (inverse of velocity unit vector)
+Vec2 Force::GenerateFrictionForce(const Particle& particle, float k) {
+    // Calculate the friction direction (inverse of velocity unit vector)
     Vec2 frictionDirection = particle.velocity.UnitVector() * -1.0;
 
-    //Calculate the friction magnitude
+    // Calculate the friction magnitude (just k, for now)
     float frictionMagnitude = k;
 
-    //Calculate the final friction force
-    frictionForce = frictionDirection * frictionMagnitude;
-
+    // Calculate the final resulting friction force vector
+    Vec2 frictionForce = frictionDirection * frictionMagnitude;
     return frictionForce;
 }
 
-Vec2 Force::GenerateSpringForce(const Particle& particle, Vec2 anchor, float restLength, float k){
-	//Calculate distance between anchor and particle
-	Vec2 d = particle.position - anchor;
+Vec2 Force::GenerateGravitationalForce(const Particle& a, const Particle& b, float G, float minDistance, float maxDistance) {
+    // Calculate the distance between the two objects
+    Vec2 d = (b.position - a.position);
 
-	//Find the spring displacement considering the rest length
-	float displacement = d.Magnitude() - restLength;
+    float distanceSquared = d.MagnitudeSquared();
 
-	//Calculate the direction of the spring force
-	Vec2 springDirection = d.UnitVector();
+    // Clamp the values of the distance (to allow for some insteresting visual effects)
+    distanceSquared = std::clamp(distanceSquared, minDistance, maxDistance);
 
-	//Calculate the magnitude of the spring force
-	float springMagnitude = -k * displacement;
+    // Calculate the direction of the attraction force
+    Vec2 attractionDirection = d.UnitVector();
 
-	//Calculate the final resulting spring force vector
-	Vec2 springForce = springDirection * springMagnitude;
-	return springForce;
+    // Calculate the strength of the attraction force
+    float attractionMagnitude = G * (a.mass * b.mass) / distanceSquared;
+
+    // Calculate the final resulting attraction force vector
+    Vec2 attractionForce = attractionDirection * attractionMagnitude;
+    return attractionForce;
 }
 
-Vec2 Force::GenerateSpringForce(const Particle& a, const Particle& b, float restLength, float k){
-	//Calculate distance between particles
-	Vec2 d= a.position - b.position;
+Vec2 Force::GenerateSpringForce(const Particle& particle, Vec2 anchor, float restLength, float k) {
+    // Calculate the distance between the anchor and the object
+    Vec2 d = particle.position - anchor;
 
-	//Find the spring displacement considering the restLength
-	float displacement = d.Magnitude() - restLength;
+    // Find the spring displacement considering the rest length
+    float displacement = d.Magnitude() - restLength;
 
-	//Calculate the direction of the spring force
-	Vec2 springDirection = d.UnitVector();
+    // Calculate the direction of the spring force
+    Vec2 springDirection = d.UnitVector();
 
-	//Calculate the magnitude of teh pring force
-	float springMagnitude = -k * displacement;
-
-	//Calculate the final resulting spring force vector
-	Vec2 springForce = springDirection * springMagnitude;
-	return springForce;
+    // Calculate the magnitude of the spring force
+    float springMagnitude = -k * displacement;
+    
+    // Calculate the final resulting spring force vector
+    Vec2 springForce = springDirection * springMagnitude;
+    return springForce;
 }
 
-Vec2 Force::GenerateGravitationalForce(const Particle& a , const Particle& b, float G, float minDistance, float maxDistance){
-	//Calculate distance between two objects
-	Vec2 d = (b.position - a.position);
+Vec2 Force::GenerateSpringForce(const Particle& a, Particle& b, float restLength, float k) {
+    // Calculate the distance between the anchor and the object
+    Vec2 d = a.position - b.position;
 
-	float distanceSquared = d.MagnitudeSquared();
-	//Clamp the value of the distance(to allow for some interesting visual effects)
-	distanceSquared = std::clamp(distanceSquared , minDistance, maxDistance);
-	//Calculate the direction of the attraction force
-	Vec2 attractionDirection = d.UnitVector();
-	//Calcualte the strength of the attraction force
-	float attractionMagnitude = G * (a.mass * b.mass) / distanceSquared;
-	//Calcualte the final resulting attraction force vector
-	Vec2 attractionForce = attractionDirection * attractionMagnitude;
-	return attractionForce;
+    // Find the spring displacement considering the rest length
+    float displacement = d.Magnitude() - restLength;
+
+    // Calculate the direction of the spring force
+    Vec2 springDirection = d.UnitVector();
+
+    // Calculate the magnitude of the spring force
+    float springMagnitude = -k * displacement;
+
+    // Calculate the final resulting spring force vector
+    Vec2 springForce = springDirection * springMagnitude;
+    return springForce;
 }
-
--- src/Physics/Particle.h
+++ src/Physics/Particle.h
@@ -4,15 +4,24 @@
 #include "Vec2.h"
 
 struct Particle {
-	int radius;
-	Vec2 position;
-	Vec2 velocity;
-	Vec2 acceleration;
+    int radius;
 
-	float mass;
+    Vec2 position;
+    Vec2 velocity;
+    Vec2 acceleration;
 
-	Particle(float x, float y, float mass);
-	~Particle();
+    Vec2 sumForces;
+
+    float mass;
+    float invMass;
+
+    Particle(float x, float y, float mass);
+    ~Particle();
+
+    void AddForce(const Vec2& force);
+    void ClearForces();
+
+    void Integrate(float dt);
 };
 
 #endif
